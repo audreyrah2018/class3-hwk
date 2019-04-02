4b2f7af28e00d5ade9c7a2460dcd1bbc36e6a1d2
Eduardo Lomonaco <lomonaco.eduardo@gmail.com>
2019-04-01 20:56:05 -0400

    Update our_csv_parser.py

--- our_csv_parser.py
+++ our_csv_parser.py
@@ -4,13 +4,6 @@ import os
 
 myfilename = "housing.data.txt"
 
-# if os.path.isfile(myfilename):
-#   print("yay, I have a file")
-#   if sky == blue:
-#     print('yay the sky is blue')
-# else:
-#   print ('boo, no files for me')
-
 with open(myfilename, 'r') as file_handle:
     for line in file_handle.readlines():
         line_clean = line.replace('   ', ' ').replace('  ', ' ')
